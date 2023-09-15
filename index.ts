fb1390242ab4aa7145e106e5354fb9ba29be67cb
Nicolas Hervy <nicolas.hervy@skf.com>
2023-09-16 01:43:41 +0200

    Working implementation

--- index.ts
+++ index.ts
@@ -1,19 +1,72 @@
-type TransformEnd = Record<string, string | number>
+import { Properties, Property } from 'csstype'
 
-type TargetElement = Record<'currentAnimation', Animation> & Element
+// =================================================================================================
+// INTERFACE
+// =================================================================================================
+
+type TransformEnd = Properties | {}
+
+interface TargetElement extends HTMLElement {
+  currentAnimation?: Animation
+}
 
 interface MicroAnimationProps {
-  debug: boolean
+  /** Output values to console.log */
+  debug?: boolean
+
+  /** Duration in ms */
   duration?: number
-  easing?: 'linear' | 'ease-in' | 'ease-out' | 'ease-in-out' | (String & {})
+
+  /** CSS easing */
+  easing?: Property.AnimationTimingFunction
+
+  /** The element to animate */
   element: TargetElement
-  fill?: 'forwards' | 'backwards' | 'both' | 'none'
+
+  /** CSS fillmode, defaults to 'forwards' */
+  fill?: FillMode
+
+  /** Object (or array of objects if keyframe animation) with CSS properties to animate to */
   transformEnd: TransformEnd | TransformEnd[]
 }
 
-function microAnimation({
+// =================================================================================================
+// IMPLEMENTATION
+// =================================================================================================
+
+/**
+ * Minimum is to pass an element and a transformEnd object.
+ * Transform start is picked up from the element's computed style.
+ *
+ * async function close() {
+ *   await microAnimation({
+ *     element,
+ *     duration: 300,
+ *     transformEnd: { opacity: 0 },
+ *   });
+ *   removeElement()
+ * }
+ *
+ * For a keyframe animation, pass an array of transformEnd objects.
+ * The offset property is optional, and defaults to 0. In the example below,
+ * the background color will change to orangered at 70% of the animation.
+ *
+ * ...
+ * await microAnimation({
+ *   element,
+ *   duration: 300,
+ *   transformEnd: [{
+ *      backgroundColor: 'orangered',
+ *      opacity: 1, offset: 0.7
+ *    }, {
+ *      transform: "translateX(0)",
+ *      backgroundColor: 'blue'
+ *    }]
+ *  ...
+ */
+export function microAnimation({
   debug = false,
-  duration = 2000,
+  duration = 300,
   easing = 'linear',
   element: element,
   fill = 'forwards',
@@ -22,27 +75,41 @@ function microAnimation({
   const transformEndArr = Array.isArray(transformEnd)
     ? transformEnd
     : [transformEnd]
+
+  // Extract the properties to animate from the transformEnd object(s)
   const targetProperties = transformEndArr.reduce(
-    (acc, keyframe) => [...acc, ...Object.keys(keyframe)],
-    [] as String[]
+    (acc, transformObj: TransformEnd) => {
+      return [...acc, ...Object.keys(transformObj)] as (keyof Properties)[]
+    },
+    [] as (keyof Properties)[]
   )
   debuglog('targetProps', targetProperties)
 
   return new Promise((resolve) => {
     element.currentAnimation?.pause()
-    const computedStyle = getComputedStyle(element)
-    const transformStart = targetProperties.reduce((acc, key) => {
-      if (key !== 'offset') acc[key] = computedStyle[key]
-      return acc
-    }, {})
+
+    /* Typescript believes getComputedStyle returns an array, workaround */
+    const computedStyle = getComputedStyle(element) as unknown as Record<
+      string,
+      string
+    >
+    const transformStart = targetProperties.reduce(
+      (acc: Record<string, string>, key: string) => {
+        if (key !== 'offset') acc[key] = computedStyle[key]?.toString()!
+        return acc as TransformEnd
+      },
+      {}
+    )
     debuglog(transformStart, transformEndArr)
-    // Handle pik up of properties from previously aborted animations
+
+    // Handle pick up of properties from previously aborted animations
     if (element.currentAnimation) {
       const timing = element.currentAnimation.effect?.getComputedTiming()
       const activeDuration = parseInt(String(timing?.activeDuration))
       const activeProgress = timing?.progress!
       duration -= activeDuration - activeProgress * activeDuration
     }
+
     element.currentAnimation?.cancel()
     element.currentAnimation = element.animate(
       [transformStart, ...transformEndArr],
